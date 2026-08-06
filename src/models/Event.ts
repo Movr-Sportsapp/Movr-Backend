73b4cb8238332dfc3c08ace88431f5a096df7e23
Riya Alex <riyaalexx@gmail.com>
2026-08-06 15:32:30 +0200

    data models created

---
+++ src/models/Event.ts
@@ -0,0 +1,91 @@
+import { Schema, model } from "mongoose";
+
+const eventSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      default: "",
+      maxlength: 1000,
+    },
+
+    sport: {
+      type: Schema.Types.ObjectId,
+      ref: "Sport",
+      required: true,
+    },
+
+    creator: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    location: {
+      city: {
+        type: String,
+        required: true,
+      },
+
+      coordinates: {
+        latitude: Number,
+        longitude: Number,
+      },
+    },
+
+    date: {
+      type: Date,
+      required: true,
+    },
+
+    skillLevel: {
+      type: String,
+      enum: ["Beginner", "Intermediate", "Advanced", "Professional"],
+      default: "Beginner",
+    },
+
+    maxParticipants: {
+      type: Number,
+      required: true,
+      min: 2,
+    },
+
+    participants: [
+      {
+        user: {
+          type: Schema.Types.ObjectId,
+          ref: "User",
+        },
+
+        joinedAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+
+    status: {
+      type: String,
+      enum: ["active", "cancelled", "completed"],
+      default: "active",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+eventSchema.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
+  transform: (_, converted) => {
+    delete (converted as Partial<typeof converted>)._id;
+  },
+});
+
+export default model("Event", eventSchema);
---
+++ src/models/index.ts
@@ -0,0 +1,3 @@
+export { default as User } from "./User.ts";
+export { default as Sport } from "./Sport.ts";
+export { default as Event } from "./Event.ts";
